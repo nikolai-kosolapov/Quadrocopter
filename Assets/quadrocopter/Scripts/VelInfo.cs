7771bb200344f996cb5a6edb0383863335bffa49
nikolai-kosolapov <kosolapov.nikolay90@ya.ru>
2016-07-27 01:19:49 -0400

    Added log for speed

--- Assets/quadrocopter/Scripts/VelInfo.cs
+++ Assets/quadrocopter/Scripts/VelInfo.cs
@@ -6,16 +6,20 @@ public class VelInfo : MonoBehaviour {
 
     public GameObject Quadrocopter;
     public GameObject Forward;
+    public GameObject Vertical;
+    public GameObject Side;
 
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       // Forward.GetComponent<Text>().text = Quadrocopter.GetComponent<QuadrocopterStabilizer2>().SpeedSensor.Forward.ToString();
+        Forward.GetComponent<Text>().text = Quadrocopter.GetComponent<QuadrocopterStabilizer2>().SpeedSensor.Forward.ToString();
+        Vertical.GetComponent<Text>().text = Quadrocopter.GetComponent<QuadrocopterStabilizer2>().SpeedSensor.Vertical.ToString();
+        Side.GetComponent<Text>().text = Quadrocopter.GetComponent<QuadrocopterStabilizer2>().SpeedSensor.Side.ToString();
 
     }
 }
